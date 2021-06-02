0bf03faa3d393b1497b6d9a3c7975141055b9f30
JacquesCorentin <74654858+JacquesCorentin@users.noreply.github.com>
2021-06-02 18:39:46 +0200

    1.3

---
+++ scenes/tiled tmx/assets_game.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="assets_game" tilewidth="64" tileheight="64" tilecount="2160" columns="60">
+ <image source="../../assets/PNG/plateforme.png" width="3840" height="2340"/>
+</tileset>
